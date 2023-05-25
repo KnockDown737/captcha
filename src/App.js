ea574544e05734f981c2fbfbeab8d8325fd83864
André Martins <percy.number2@gmail.com>
2023-05-24 23:15:34 -0300

    inicializing project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import Vitoria from "./vitoria";
+import "./App.css";
 
 function App() {
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+  const [redirectTo, setRedirectTo] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (recaptchaValue) {
+      // ReCAPTCHA foi preenchido corretamente, redirecionar para outra página
+      console.log("ReCAPTCHA preenchido corretamente");
+      setRedirectTo("/vitoria"); // Define a página de redirecionamento
+    } else {
+      // ReCAPTCHA não foi preenchido, exibe uma mensagem de erro
+      alert("Por favor, preencha o ReCAPTCHA");
+    }
+  };
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+  };
+
+  if (redirectTo) {
+    return <Vitoria />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canva">
+      <center>
+        <h1>Formulário</h1>
+      </center>
+
+      <div class="container">
+        <form class="formulario" onSubmit={handleSubmit}>
+          <div class="forms">
+            <div class="formsContainer">
+              <label class="tituloInput">Email:</label>
+              <input type="email" placeholder="Email" name="email"  class="input" required />
+              <br />
+              <label class="tituloInput">Senha:</label>
+              <input type="password" placeholder="Senha" name="senha" class="input"   required />
+            </div>
+            <ReCAPTCHA
+              sitekey="6LehEyYmAAAAALYZ4iYcCr_-c7wN63YDs4BH66Y0"
+              onChange={handleRecaptchaChange}
+              id="captcha"
+            />
+            <button type="submit" class="btn">Login</button>
+            
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
---
+++ src/vitoria.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Vitoria = () => {
+    return <h1>FUNCIONA!!!</h1>;
+  };
+  
+export default Vitoria;
